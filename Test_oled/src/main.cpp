0a663161eda4c763e546f296fcb0400b01ee0700
nhatlinhsayhi <nhatlinhphn@gmail.com>
2026-09-18 00:13:09 +0700

    test_oled v3

--- Test_oled/src/main.cpp
+++ Test_oled/src/main.cpp
@@ -11,12 +11,6 @@ void setup() {
 }
 
 void loop() {
-   // Hiển thị màn hình Unknown
-OLED_Show_ReplyOK();
-delay(3000); // Giữ màn hình trong 3 giây
-// Hoặc kết quả cuối cùng là Unknown
-OLED_Show_NoReply(); 
-delay(3000); // Giữ màn hình trong 3 giây
-    
+  OLED_Show_Standby();
 
 }
